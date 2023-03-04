22d6aff58571696392993eff9b515caa933a8e0f
LAPTOP-8L5ICCUP\User <nur12345@internet.ru>
2023-03-04 17:49:07 +0300

    dz1

--- src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
+++ src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import s from './FriendMessage.module.css'
+import {MessageType} from "../HW1";
+
+export type FriendMessagePropsType = {
+    message: MessageType
+}
 
 // создать тип вместо any и отобразить приходящие данные
-const FriendMessage = (props: any) => {
+const FriendMessage = (props: FriendMessagePropsType) => {
     return (
         <div
             id={'hw1-friend-message-' + props.message.id}
             className={s.friendMessage}
         >
             <div className={s.friendImageAndText}>
-                <img
+                <img src={props.message.user.avatar}
                     id={'hw1-friend-avatar-' + props.message.id}
                     // создаёт студент
 
@@ -20,16 +25,18 @@ const FriendMessage = (props: any) => {
                         id={'hw1-friend-name-' + props.message.id}
                         className={s.friendName}
                     >
-                        {/*создаёт студент*/}
 
+                        {/*создаёт студент*/}
+                        {props.message.user.name}
                         {/**/}
                     </div>
                     <pre
                         id={'hw1-friend-text-' + props.message.id}
                         className={s.friendMessageText}
                     >
-                        {/*создаёт студент*/}
 
+                        {/*создаёт студент*/}
+                        {props.message.message.text}
                         {/**/}
                     </pre>
                 </div>
@@ -38,6 +45,7 @@ const FriendMessage = (props: any) => {
                 id={'hw1-friend-time-' + props.message.id}
                 className={s.friendTime}
             >
+                {props.message.message.time}
                 {/*создаёт студент*/}
 
                 {/**/}
